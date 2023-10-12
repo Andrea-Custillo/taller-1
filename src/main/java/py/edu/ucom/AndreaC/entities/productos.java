5ef3c62012ee7681caad20705f5634a0fb08ec87
Andrea-Custillo <145853342+Andrea-Custillo@users.noreply.github.com>
2023-10-11 21:46:06 -0300

    parcial

---
+++ src/main/java/py/edu/ucom/AndreaC/entities/productos.java
@@ -0,0 +1,30 @@
+package py.edu.ucom.AndreaC.entities;
+
+public class productos {
+    public Integer codigo;
+    public String nombreProducto;
+    public Integer precio;
+    
+    public Integer getCodigo() {
+        return codigo;
+    }
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+    public Integer getPrecio() {
+        return precio;
+    }
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+    public void setPrecio(Integer precio) {
+        this.precio = precio;
+    }
+
+
+    
+
+}
--- src/main/java/py/edu/ucom/AndreaC/utils/DataSourceJSON.java
+++ src/main/java/py/edu/ucom/AndreaC/utils/DataSourceJSON.java
@@ -12,12 +12,27 @@ import py.edu.ucom.AndreaC.entities.biblioteca.Libros;
 @ApplicationScoped
 
 public class DataSourceJSON {
+public void guardarLibro(Libros libros){
 
+    try {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Libros> lista = obtenerLibros();
+        lista.add( libros);
+        mapper.writeValue(new File ("C:\\Users\\Andrea\\Desktop\\TALLER\\taller-1\\src\\main\\java\\py\\edu\\ucom\\AndreaC\\utils\\libros.json")))
+    } catch (Exception e) {
+        // TODO: handle exception
+    }
+
+    List<Libros> lista = obtenerLibros();
+
+
+}
   public List<Libros> obtenerLibros(){
         ObjectMapper mapper = new ObjectMapper();
         List<Libros> libros = new ArrayList<>();
         try {
-            libros = mapper.readValue(new File ("libros.json"), new TypeReference<List<Libros>>(){});
+            libros = mapper.readValue(
+                new File ("C:\\Users\\Andrea\\Desktop\\TALLER\\taller-1\\src\\main\\java\\py\\edu\\ucom\\AndreaC\\utils\\libros.json"), new TypeReference<List<Libros>>(){});
 
             
         } catch (Exception e) {
